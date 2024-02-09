d779fd478e5229a73a1fbfd493767dca69fde930
Jason <jasonresquilona@gmail.com>
2024-02-08 22:56:23 -0800

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'LoginPage', component:LoginPageComponent},
+];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon'
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
